81d4e45e7fa355e56d87064512f3654ba95c706f
xylnx <xylnx@mailbox.org>
2022-11-22 18:20:23 +0100

    Rewrite home view

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,12 @@ import { HistoricCoinPrice } from './HistoricCoinPrice';
 import { LayoutCard } from './LayoutCard';
 import { Button } from './Button';
 
+import { ComparePrices } from './ComparePrices';
+import { Chart } from './Chart';
+import { PriceTable } from './PriceTable';
+
+import './Home.scss';
+
 const Home = () => {
   const getDate = () => {
     const date = new Date();
@@ -11,18 +17,24 @@ const Home = () => {
   };
 
   return (
-    <div className="home flow">
-      <h1>
-        Current Currency Prices:{' '}
-        <span className="text-accent">{getDate()}</span>
-      </h1>
-      <LayoutCard cardTitle="Bitcoin">
-        <CoinPrice coin="bitcoin" />
+    <div className="home">
+      <LayoutCard cardTitle="Current Prices" classNames="current-prices">
+        <div className="current-price">
+          <span className="current-price__coin-name">Bitcoin</span>
+          <CoinPrice coin="bitcoin" />
+        </div>
+        <div className="current-price">
+          <span className="current-price__coin-name">Ethereum</span>
+          <CoinPrice coin="ethereum" />
+        </div>
       </LayoutCard>
-      <LayoutCard cardTitle="Ethereum">
-        <CoinPrice coin="ethereum" />
+      <ComparePrices />
+      <LayoutCard cardTitle="Chart" classNames="chart" fullWidth={true}>
+        <Chart />
       </LayoutCard>
-      <Button btnDest="/compare" btnCopy="Compare with historic price data" />
+      <LayoutCard cardTitle="Table" classNames="table" fullWidth={true}>
+        <PriceTable />
+      </LayoutCard>
     </div>
   );
 };
