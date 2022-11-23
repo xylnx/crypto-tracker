76abc554c230c2cda8c753835e8b16c0ada902fe
xylnx <xylnx@mailbox.org>
2022-11-23 22:25:06 +0100

    Clean up App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import logo from './logo.svg';
-import './App.css';
-
 // Hooks
-import { useState, useEffect } from 'react';
-
 // Components
 import { Home } from './components/Home';
-import { ComparePrices } from './components/ComparePrices';
-import { PriceTable } from './components/PriceTable';
-import { Chart } from './components/Chart';
 
 function App() {
-  const [coin, setCoin] = useState(null);
-  const [date, setDate] = useState('');
-
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route path="/compare" element={<ComparePrices />} />
-          <Route path="/price-table" element={<PriceTable />} />
-          <Route path="/chart" element={<Chart />} />
         </Routes>
       </BrowserRouter>
     </div>
